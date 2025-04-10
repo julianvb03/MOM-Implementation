8a194523c4f4f83751002e576600e61649a7a3e2
Kevin Quiroz <kquirozg@eafit.edu.co>
2025-04-09 19:21:13 -0500

    Zookeeper initial test

--- server/app/config/env.py
+++ server/app/config/env.py
@@ -19,3 +19,9 @@ REDIS_PORT = os.getenv('REDIS_PORT')
 # Default user configuration
 DEFAULT_USER_PASSWORD = os.getenv('DEFAULT_USER_PASSWORD')
 DEFAULT_USER_NAME = os.getenv('DEFAULT_USER_NAME')
+
+# Node configuration
+NODE_ID = os.getenv("NODE_ID", "mom-unknown")
+NODE_HOST = os.getenv("NODE_HOST", "http://localhost:8000")
+ZOOKEEPER_HOST = os.getenv("ZOOKEEPER_HOST", "localhost")
+ZOOKEEPER_PORT = os.getenv("ZOOKEEPER_PORT", "2181")
